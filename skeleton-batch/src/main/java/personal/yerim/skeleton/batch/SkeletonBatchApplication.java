5515be4d033c2b7004da574e7cfc42228e3b7de7
yerimkim <kiel0103@naver.com>
2024-02-24 12:11:12 +0900

    feat: batch 설정

--- skeleton-batch/src/main/java/personal/yerim/skeleton/batch/SkeletonBatchApplication.java
+++ skeleton-batch/src/main/java/personal/yerim/skeleton/batch/SkeletonBatchApplication.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -11,7 +12,12 @@ import org.springframework.context.annotation.ComponentScan;
 public class SkeletonBatchApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(SkeletonBatchApplication.class, args);
+    System.setProperty("spring.devtools.restart.enabled", "false");
+    System.setProperty("user.timezone", "Asia/Seoul");
+
+    ConfigurableApplicationContext applicationContext =
+        SpringApplication.run(SkeletonBatchApplication.class, args);
+    System.exit(SpringApplication.exit(applicationContext));
   }
 
   @PostConstruct
